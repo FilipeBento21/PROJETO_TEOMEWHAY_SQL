f23f450c482e8ac5f21b146a8009c88ebc7f9fd8
FilipeBento21 <filipebentoheliodoro@gmail.com>
2025-03-08 15:00:49 -0300

    Filtrando Linhas de cada Tabela
    Aqui estamos filtrando as linhas e informações necessárias para fazer as consultas das informações necessárias, bom para não deixar as informações necessárias no meio de uma multidão de informações sem necessidade.

--- ep04/join.sql
+++ ep04/join.sql
@@ -5,9 +5,24 @@
 --TABELA DE VENDAS;
 --SELECT * FROM tb_orders
 
+/*
 ------------JOIN-------------
 SELECT *
 
+FROM tb_order_items AS Tabela1
+
+LEFT JOIN tb_products AS Tabela2
+ON Tabela1.product_id = Tabela2.product_id
+*/
+
+-----------buscando informações necessárias de cada tabela
+
+SELECT 
+        Tabela2.product_category_name,
+        Tabela1.order_id,
+        Tabela1.order_item_id,
+        Tabela1.product_id,
+        Tabela1.price
 FROM tb_order_items AS Tabela1
 
 LEFT JOIN tb_products AS Tabela2
