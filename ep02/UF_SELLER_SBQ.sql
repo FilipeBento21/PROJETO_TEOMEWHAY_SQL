c9bb7ba5e11cdb1d59041c7b729fd9d61aad3708
FilipeBento21 <filipebentoheliodoro@gmail.com>
2025-06-24 01:14:50 -0300

    REFORÇANDO O GROUP BY  -  DESCREVENDO SUBQUERY
    Conseguimos reforçar o comando group by e tivemos a necessidade de fazer um filtro após o agrupamento e vimos que o where não funciona após o group by e procuramos uma saída e o primeiro explicado na aula foi o conceito de subquerys

---
+++ ep02/UF_SELLER_SBQ.sql
@@ -0,0 +1,21 @@
+
+select *
+
+from (
+
+select seller_state,
+        count(*) as qtde_sellers
+
+from tb_sellers
+
+-- uSAR IN PARA FILTRAR MAIS DE UM ESTADO DE FORMA ORGANIZADA
+
+-- where seller_state in ('SP','RJ','PR')
+
+
+
+group by seller_state
+
+)
+
+where qtde_sellers > 10
